cc03b32e0dad8b28aa939c03dc0268daeb902b31
magkuette <153895391+magkuette@users.noreply.github.com>
2024-05-09 04:31:10 -0400

    Update test_finalproject326.py

--- test_finalproject326.py
+++ test_finalproject326.py
@@ -233,29 +233,41 @@ Returns:
 # A "Back to Options" button and a "Log out" button
 # You can now either close out the GUI window and rerun the code to launch the GUI again, press "Back to Options" to test Test Case #1 for options, or press "Log out" to test Test Case #1 or Test Case #2 for get_user_info function
 
+""" Test Case #1 for threshold_reminders function: This is a test case to cover the fifth page (self.p5) of the tkinter GUI for
+    finalproject326.py. We are testing this function to show that clicking the "Show thresholds passed" button works as expected.
 
+    Returns:
+        thresholds passed
+"""
+# This test case assumes the code is already running and you have gone through either Test Case #1 or Test Case #2 for the get_user_info function and Test Case #1 for options function
 
+# To test this function, please do the following:
 
+# Input a number for "Dining Dollar Plan" on self.p1
+# Input "0" for "Current Dining Dollar Balance" on self.p1
+# Navigate to the options page (self.p3) and click the "Show thresholds passed"
+# Check that it says "You have used up all of your dining dollars. Don't worry!"
+# You can now either close out the GUI window and rerun the code to launch the GUI again, press "Back to Options" to test Test Case #1 for options, or press "Log out" to test Test Case #1 or Test Case #2 for get_user_info function
 
+# Input a number for "Dining Dollar Plan" and a number for "Current Dining Dollar Balance" on self.p1 that corresponds to this equation: (balance - dining dollar plan)/dining dollar plan = a number from 0.75 to 0.99
+# Navigate to the options page (self.p3) and click the "Show thresholds passed"
+# Check that it says "You have passed the 25th, 50th, and 75th percentiles of total dining dollars in your plan. You have used up [a number from 0.75 to 0.99 (multipled by 100)]% of all of your dining dollars.
+# You can now either close out the GUI window and rerun the code to launch the GUI again, press "Back to Options" to test Test Case #1 for options, or press "Log out" to test Test Case #1 or Test Case #2 for get_user_info function
 
+# Input a number for "Dining Dollar Plan" and a number for "Current Dining Dollar Balance" on self.p1 that corresponds to this equation: (balance - dining dollar plan)/dining dollar plan = a number from 0.5 to 0.74
+# Navigate to the options page (self.p3) and click the "Show thresholds passed"
+# Check that it says "You have passed the 25th, and 50th percentiles of total dining dollars in your plan. You have used up [a number from 0.5 to 0.74 (multipled by 100)]% of all of your dining dollars.
+# You can now either close out the GUI window and rerun the code to launch the GUI again, press "Back to Options" to test Test Case #1 for options, or press "Log out" to test Test Case #1 or Test Case #2 for get_user_info function
 
+# Input a number for "Dining Dollar Plan" and a number for "Current Dining Dollar Balance" on self.p1 that corresponds to this equation: (balance - dining dollar plan)/dining dollar plan = a number from 0.5 to 0.49
+# Navigate to the options page (self.p3) and click the "Show thresholds passed"
+# Check that it says "You have passed the 25th percentile of total dining dollars in your plan. You have used up [a number from 0.25 to 0.49 (multipled by 100)]% of all of your dining dollars.
+# You can now either close out the GUI window and rerun the code to launch the GUI again, press "Back to Options" to test Test Case #1 for options, or press "Log out" to test Test Case #1 or Test Case #2 for get_user_info function
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Input a number for "Dining Dollar Plan" and a number for "Current Dining Dollar Balance" on self.p1 that corresponds to this equation: (balance - dining dollar plan)/dining dollar plan = a number from 0 to 0.24
+# Navigate to the options page (self.p3) and click the "Show thresholds passed"
+# Check that it says "You have passed no remarkable thresholds of spent dining dollars in your plan yet. You have used up [a number from 0 to 0.24]% of all of your dining dollars.
+# You can now either close out the GUI window and rerun the code to launch the GUI again, press "Back to Options" to test Test Case #1 for options, or press "Log out" to test Test Case #1 or Test Case #2 for get_user_info function
 
 
 """ Test Case #1 for suggested_spending function: This is a test case to cover the sixth page of the tkinter GUI for
